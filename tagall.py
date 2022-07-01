8a3d8f7b5a5a4fe62df313416dcc8860ebf6beef
elieve <bayustiadi@ymail.com>
2022-07-01 12:00:27 +0800

    Update tagall.py

--- tagall.py
+++ tagall.py
@@ -128,3 +128,7 @@ Only admins can tag all.  here is a list of commands
 ❂ /tagall or @all (reply to message or add another message) To mention all members in your group, without exception.
 ❂ /cancel for canceling the mention-all.
 """
+
+print("PingAll is alive!")  
+Client.run()
+
